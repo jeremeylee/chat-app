383f4b23c48a73ec3e754622c72f37a7770aa4ab
Jeremey Lee <leejeremey@gmail.com>
2019-08-01 18:44:41 -0700

    new messages appear on screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 import ChatInput from './components/chatInput';
 import messageHelper from './components/messages_helper';
 import Messages from './components/messages';
 
 const App = (props) => {
+  useEffect(() => {
+    showMessages();
+  }, [props.message]);
 
   const showMessages = () => (
-    messageHelper.map(message => (
+    props.message.map(message => (
       <Messages
       //implement key when IDs are implemented 
         username={message.username}
@@ -18,10 +22,18 @@ const App = (props) => {
 
   return (
     <div>
-      {showMessages()}
+      <div className='message-container'>
+        {showMessages()}
+      </div>
       <ChatInput />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => (
+  {
+    message: state.message,
+  }
+);
+
+export default connect(mapStateToProps)(App);
