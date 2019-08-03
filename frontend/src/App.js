f7f538e0a8ee9d07204e93d765beb3cc7c59f524
Jeremey Lee <leejeremey@gmail.com>
2019-08-02 17:05:26 -0700

    created custom context menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 import ChatInput from './components/chatInput';
@@ -8,11 +8,16 @@ import { messageAction } from './reducers/messageReducer';
 import messageService from './services/messages';
 
 const App = (props) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [left, setLeft] = useState(0);
+  const [top, setTop] = useState(0);
+
   const { messageAction, message} = props;
 
   const divRef = React.createRef(); //Used to ref the hidden <div> at the bottom of the message container and keep the chat box always scrolled down
-  
+
   useEffect(() => {
+    window.addEventListener('click', () => setShowMenu(false));
     const fetchMessages = async () => {
       const savedMessages = await messageService.getMessages();
       messageAction('NEW', savedMessages);
@@ -22,18 +27,46 @@ const App = (props) => {
   
   useEffect(() => {
     divRef.current.scrollIntoView();
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [message]);
 
+  useEffect(() => {
+    // Needed to prevent double contextmenu caused by a second right click
+      window.addEventListener('contextmenu', event => {
+        event.preventDefault();
+        console.log('hello');
+      })
+    
+  }, [showMenu]);
+
+  const handleEdit = () => {
+    //console.log('edit');
+  }
+
+  const handleDelete = () => {
+   // console.log('delete');
+  }
+
+  const menuStyle = {
+    display: showMenu ? '' : 'none',
+    position: 'absolute',
+    left: `${left}px`,
+    top: `${top}px`,
+  }
   const showMessages = () => {
     if (message) {
       return (
       message.map(content => (
         <Message
-        //implement key when IDs are implemented 
+          key={content.id}
           username={content.username}
           message={content.message}
+          showMenu={showMenu}
+          setShowMenu={setShowMenu}
+          left={left}
+          setLeft={setLeft}
+          top={top}
+          setTop={setTop}
         />
       ))
       );
@@ -46,7 +79,14 @@ const App = (props) => {
         {showMessages()}
         <div ref={divRef}></div>
       </div>
+      <div style={menuStyle}>
+        <ul>
+          <li>Edit</li>
+          <li>Delete</li>
+        </ul>
+      </div>
       <ChatInput />
+
     </div>
   );
 }
--- frontend/src/components/message.js
+++ frontend/src/components/message.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { Row } from 'antd';
 
 const Messages = (props) => {
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+
+    props.setShowMenu(true);
+    props.setLeft(event.clientX)
+    props.setTop(event.clientY)
+    console.log(event);
+    
+  }
+/* 
+  const menuStyle = {
+    display: showMenu ? '' : 'none'
+  }
+   */
   return (
     <div>
       <Row>
         <strong>{props.username}</strong>
-        <p>{props.message}</p>
+        <div onContextMenu={handleContextMenu}><p>{props.message}</p></div>
       </Row>
     </div>
   )
