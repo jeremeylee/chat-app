a0284051ac0422e9ecf9e7cda0e2a910ac2c30d1
Jeremey Lee <leejeremey@gmail.com>
2019-08-03 23:35:11 -0700

    set up basic GET request

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
