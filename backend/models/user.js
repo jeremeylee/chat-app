08ddb125fe48b13258e14a6ac9525f94575b9726
Jeremey Lee <leejeremey@gmail.com>
2019-08-04 01:10:44 -0700

    created user schemaa

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+userSchema.plugin(uniqueValidator);
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  };
+});
+
+module.exports = mongoose.model('User', userSchema);
